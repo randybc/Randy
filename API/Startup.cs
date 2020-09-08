f79c8242fae8eb39a67c11167fda795163c0384a
Randall Badilla Castro <randall.bc@hp.com>
2020-09-08 00:47:42 -0600

    adding CORS policy

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,13 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -48,6 +55,8 @@ namespace API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -25,7 +25,7 @@ class App extends Component {
       <img src={logo} className="App-logo" alt="logo" />
       <ul>
       {this.state.values.map((value: any) => (
-        <li>{value.name}</li>
+        <li key={value.id} >{value.name}</li>
         ))}
         </ul>
         </header>
