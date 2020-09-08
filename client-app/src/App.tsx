e4316cb3fb542c945ff36bee9dee6efdc690373c
Randall Badilla Castro <randall.bc@hp.com>
2020-09-08 00:31:03 -0600

    installed axios, from mod 3, cap 30

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,32 +1,37 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
   state = {
-      values: []
+    values: []
   }
-
+  
   componentDidMount() {
-    this.setState({
-      values: [{id:1, name: 'Value 101'}, {id:2, name: 'Value 102'}]
+    axios.get('http://localhost:5000/api/values')
+    .then ((response) => {
+      console.log(response);
+      this.setState({
+        values: response.data
+      })
     })
   }
-
+  
   render() {
-      return (
+    return (
       <div className="App">
       <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <ul>
-        {this.state.values.map((value: any) => (
-          <li>{value.name}</li>
+      {this.state.values.map((value: any) => (
+        <li>{value.name}</li>
         ))}
-      </ul>
-      </header>
-      </div>
-      );
+        </ul>
+        </header>
+        </div>
+        );
+      }
     }
-  }
-  export default App;
-  
+    export default App;
+    
