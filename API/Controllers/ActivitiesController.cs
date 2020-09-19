7f42355d79efd85f249a38038be2be5ee851b79f
Randall Badilla Castro <randall.bc@hp.com>
2020-09-18 23:48:57 -0600

    End of seccion 4

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,4 +1,3 @@
-using System.Dynamic;
 using System.Collections.Generic;
 using MediatR;
 using System.Threading.Tasks;
@@ -36,6 +35,19 @@ namespace API.Controllers
         public async Task<ActionResult<Unit>> Create(Create.Command command)
         {
             return await _mediator.Send(command);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
+        {
+            command.Id = id;
+            return await _mediator.Send(command);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Unit>> Delete(Guid id)
+        {
+            return await _mediator.Send(new Delete.Command { Id = id });
         }
     }
 }
